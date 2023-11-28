c17dc3b3b435bfc02d3480663d66c7130dc71c20
Letty Makore <lettymakore1@gmail.com>
2023-11-28 20:55:59 +0200

    web#

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,50 @@
+# Ensure Nginx package is installed
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Configure the Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+    root /var/www/html;
+    index index.html;
+    
+    location / {
+        try_files \$uri \$uri/ =404;
+        add_header Content-Type text/html;
+        echo 'Hello World!';
+    }
+    
+    location /redirect_me {
+        return 301 http://example.com;
+    }
+}
+  ",
+  require => Package['nginx'],
+}
+
+# Create the Hello World HTML file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Create a symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx is running and enabled at boot
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  require => [File['/etc/nginx/sites-enabled/default'], File['/var/www/html/index.html']],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,1 @@
+0x0C-web_server is a simple web server project designed for educational purposes. It provides a basic understanding of how web servers work by implementing a minimalistic server in C.
